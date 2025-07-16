48794790198d8d6b47d1c283141804374dfd80b9
Marcus Carneiro <marcusvcarneiro@noreply.github.com>
2025-07-16 16:47:44 +0100

    csv-export

--- StructureResultsViewer/StructureResultsViewer/Controller.cs
+++ StructureResultsViewer/StructureResultsViewer/Controller.cs
@@ -8,6 +8,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml.Linq;
 
 
@@ -16,25 +18,263 @@ namespace StructureResultsViewer
     public class Controller
     {
         private AnalysisResult? _analysisResult;
-
-
+        private HashSet<int> _elemSelection;
+        private HashSet<int> _nodeSelection;
+        private Dictionary<Guid, int> _guidGsaKeys;
         public AnalysisResult? Results { get { return _analysisResult; } }
 
+        public Controller()
+        {
+            _elemSelection = new HashSet<int>();
+            _nodeSelection = new HashSet<int>();
+            _guidGsaKeys = new Dictionary<Guid, int>();
+        }
 
-        public Controller() { }
+        public void SetNodeSelection(HashSet<int> elemSelection)
+        {
+            if ((_analysisResult == null) || (_guidGsaKeys == null))
+                return;
+            _nodeSelection.Clear();
+            _elemSelection = new HashSet<int>(elemSelection);
+            foreach (Element1d element1D in _analysisResult.StructureElements)
+            {
+                if (elemSelection.Contains(element1D.GsaModelKey))
+                {
+                    ElementEndNode node0 = element1D.Nodes.First();
+                    ElementEndNode node1 = element1D.Nodes.Last();
+                    _nodeSelection.Add(_guidGsaKeys[node0.NodeId]);
+                    _nodeSelection.Add(_guidGsaKeys[node1.NodeId]);
+                }
+            }
+        }
 
+        public bool IsSelected(int nodeIndex)
+        {
+            return _nodeSelection.Contains(nodeIndex);
+        }
+
+        // Clearing selection means all nodes get displayed.
+        public void ClearNodeSelection()
+        {
+            _nodeSelection.Clear();
+            if ((_analysisResult != null) && (_analysisResult.Nodes != null))
+            {
+                foreach (var kvp in _analysisResult.Nodes)
+                {
+                    _nodeSelection.Add(kvp.Value.GsaModelKey);
+                }
+            }
+        }
+        public void ClearElementSelection()
+        {
+            _elemSelection.Clear();
+            if ((_analysisResult != null) && (_analysisResult.Nodes != null))
+            {
+                foreach (var elem in _analysisResult.StructureElements)
+                {
+                    _elemSelection.Add(elem.GsaModelKey);
+                }
+            }
+        }
+
+        public void UpdateNodeTable()
+        {
+            if (_analysisResult != null)
+            {
+                _guidGsaKeys = new Dictionary<Guid, int>(_analysisResult.Nodes.Count);
+
+                foreach (Node node in _analysisResult.Nodes.Values)
+                {
+                    _guidGsaKeys.Add(node.Id, node.GsaModelKey);
+                }
+            }
+        }
 
         public void LoadResultsJson(FileInfo resultsFile)
         {
             if (resultsFile != null)
             {
-                string json = File.ReadAllText(resultsFile.ToString());
+                String json = File.ReadAllText(resultsFile.ToString());
                 if (resultsFile.DirectoryName != null)
                 {
                     var dir = new DirHelper(resultsFile.DirectoryName.ToString());
                     _analysisResult = dir.ReadData<AnalysisResult>(resultsFile.Name);
                 }
+                UpdateNodeTable();
+                ClearNodeSelection();
+                ClearElementSelection();
             }
+        }
+        private double GetResult(String selectedResult, ContactInterface CIResult, int index)
+        {
+            if ((selectedResult == "Reactions (contacts)") || (selectedResult == "Relative displacement (contacts)"))
+            {
+                List<Displacement> interfaceResult;
+                if (selectedResult == "Reactions (contacts)")
+                {
+                    interfaceResult = CIResult.Reaction;
+                }
+                else
+                {
+                    interfaceResult = CIResult.RelativeDisplacement;
+                }
+                return GetValue(interfaceResult[index], selectedResult);
+            }
+            else
+            {
+                if (selectedResult == "Steady pore-pressure (contacts)")
+                {
+                    return CIResult.SteadyStatePorePressure[index];
+                }
+                else
+                {
+                    return CIResult.ExcessPorePressure[index];
+                }
+            }
+        }
+
+        private double GetValue(Displacement displacement, string direction)
+        {
+            if (direction == "X")
+                return displacement.X;
+            else if (direction == "Y")
+                return displacement.Y;
+            else
+                return Math.Sqrt(displacement.X * displacement.X + displacement.Y * displacement.Y);
+        }
+
+        public List<(int, double, double)> ReadContactResults(String selectedResult)
+        {
+            List<(int, double, double)> contactResults = new List<(int, double, double)>();
+            const int numSides = 2;
+            if (_analysisResult == null)
+            {
+                return contactResults;
+            }
+
+            Dictionary<Guid, int> guidGsaKeys = new Dictionary<Guid, int>(_analysisResult.Nodes.Count);
+
+            foreach (Node node in _analysisResult.Nodes.Values)
+            {
+                guidGsaKeys.Add(node.Id, node.GsaModelKey);
+            }
+
+            int gsaKeyRefNode = guidGsaKeys[_analysisResult.StructureElements[0].Nodes[0].NodeId];
+            double x0 = _analysisResult.Nodes[gsaKeyRefNode].X;
+            double y0 = _analysisResult.Nodes[gsaKeyRefNode].Y;
+
+            HashSet<Guid> structureNodes = new HashSet<Guid>();
+            for (int side = 0; side < numSides; side++)
+            {
+                foreach (Element1d element1D in _analysisResult.StructureElements)
+                {
+                    if (_elemSelection.Contains(element1D.GsaModelKey))
+                    {
+                        ElementEndNode node0 = element1D.Nodes[0];
+                        ElementEndNode node1 = element1D.Nodes[1];
+
+                        (double, double) pointNode0 = (0, 0), pointNode1 = (0, 0), midPoint = (0, 0);
+                        if (_analysisResult.Nodes.TryGetValue(guidGsaKeys[node0.NodeId], out Node? value0))
+                        {
+                            pointNode0 = (value0.X, value0.Y);
+                        }
+
+                        if (_analysisResult.Nodes.TryGetValue(guidGsaKeys[node1.NodeId], out Node? value1))
+                        {
+                            pointNode1 = (value1.X, value1.Y);
+                        }
+                        midPoint = ((pointNode0.Item1 + pointNode1.Item1) / 2.0, (pointNode0.Item2 + pointNode1.Item2) / 2.0);
+
+                        for (int segment = 0; segment < 2; segment++)
+                        {
+                            ContactInterface CIResult = element1D.GetContactResult(side, segment);
+                            if (segment == 0)
+                            {
+                                double resultNode0 = GetResult(selectedResult, CIResult, 0);
+                                double resultMidNode = GetResult(selectedResult, CIResult, 1);
+
+                                double lengthNode0 = Math.Sqrt((pointNode0.Item1 - x0) * (pointNode0.Item1 - x0) + (pointNode0.Item2 - y0) * (pointNode0.Item2 - y0));
+                                double lengthMidNode = Math.Sqrt((midPoint.Item1 - x0) * (midPoint.Item1 - x0) + (midPoint.Item2 - y0) * (midPoint.Item2 - y0));
+
+                                contactResults.Add((side, lengthNode0, resultNode0));
+                                contactResults.Add((side, lengthMidNode, resultMidNode));
+
+                            }
+                            else
+                            {
+                                double resultMidNode = GetResult(selectedResult, CIResult, 0);
+                                double resultNode1 = GetResult(selectedResult, CIResult, 1);
+
+                                double lengthNode1 = Math.Sqrt((pointNode1.Item1 - x0) * (pointNode1.Item1 - x0) + (pointNode1.Item2 - y0) * (pointNode1.Item2 - y0));
+                                double lengthMidNode = Math.Sqrt((midPoint.Item1 - x0) * (midPoint.Item1 - x0) + (midPoint.Item2 - y0) * (midPoint.Item2 - y0));
+                                contactResults.Add((side, lengthMidNode, resultMidNode));
+                                contactResults.Add((side, lengthNode1, resultNode1));
+
+                            }
+                        }
+                    }
+                }
+            }
+            return contactResults;
+        }
+
+        public List<(double, double)> ReadNodalResults(String selectedResult)
+        {
+            List<(double, double)> nodalResults = new List<(double, double)>();
+            HashSet<Guid> structureNodes = new HashSet<Guid>();
+
+            if (_analysisResult == null)
+                return nodalResults;
+
+            int gsaKeyRefNode = _guidGsaKeys[_analysisResult.StructureElements[0].Nodes[0].NodeId];
+            double x0 = _analysisResult.Nodes[gsaKeyRefNode].X;
+            double y0 = _analysisResult.Nodes[gsaKeyRefNode].Y;
+
+            foreach (Element1d element1D in _analysisResult.StructureElements)
+            {
+                foreach (ElementEndNode node in element1D.Nodes)
+                {
+                    int gsaModelKey = _guidGsaKeys[node.NodeId];
+                    if (_nodeSelection.Contains(gsaModelKey))
+                    {
+                        if (!structureNodes.Contains(node.NodeId))
+                        {
+                            structureNodes.Add(node.NodeId);
+                        }
+                    }
+                }
+            }
+            foreach (Guid guid in structureNodes)
+            {
+                if (_analysisResult.Nodes.TryGetValue(_guidGsaKeys[guid], out Node? value))
+                {
+                    double length = Math.Sqrt((value.X - x0) * (value.X - x0) + (value.Y - y0) * (value.Y - y0));
+                    double result = 0;
+                    switch (selectedResult)
+                    {
+                        case "Total displacement":
+                            result = GetValue(value.Displacement, selectedResult);
+                            break;
+                        case "Stage displacement":
+                            result = GetValue(value.StageDisplacement, selectedResult);
+                            break;
+                        case "Bending moment":
+                            result = value.BendingMoment ?? 0;
+                            break;
+                        case "Axial force":
+                            result = value.AxialForce ?? 0;
+                            break;
+                        case "Shear force":
+                            result = value.ShearForce ?? 0;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    nodalResults.Add((length, result));
+                }
+            }
+            return nodalResults;
         }
     }
 }
--- StructureResultsViewer/StructureResultsViewer/PlotControl.cs
+++ StructureResultsViewer/StructureResultsViewer/PlotControl.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace StructureResultsViewer
 {
@@ -58,43 +59,9 @@ namespace StructureResultsViewer
             }
         }
 
-        private double GetValue(Displacement displacement, string direction)
-        {
-            if (direction == "X")
-                return displacement.X;
-            else if (direction == "Y")
-                return displacement.Y;
-            else
-                return Math.Sqrt(displacement.X * displacement.X + displacement.Y * displacement.Y);
-        }
 
-        private double GetResult(ContactInterface CIResult, int index)
-        {
-            if ((_selected_result == "Reactions (contacts)") || (_selected_result == "Relative displacement (contacts)"))
-            {
-                List<Displacement> interfaceResult;
-                if (_selected_result == "Reactions (contacts)")
-                {
-                    interfaceResult = CIResult.Reaction;
-                }
-                else
-                {
-                    interfaceResult = CIResult.RelativeDisplacement;
-                }
-                return GetValue(interfaceResult[index], _selected_direction);
-            }
-            else
-            {
-                if (_selected_result == "Steady pore-pressure (contacts)")
-                {
-                    return CIResult.SteadyStatePorePressure[index];
-                }
-                else
-                {
-                    return CIResult.ExcessPorePressure[index];
-                }
-            }
-        }
+
+
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
@@ -130,71 +97,28 @@ namespace StructureResultsViewer
             if (ModelController.Results == null)
                 return;
 
-            Dictionary<Guid, int> guidGsaKeys = new Dictionary<Guid, int>(ModelController.Results.Nodes.Count);
+            List<(double, double)> nodalResults = ModelController.ReadNodalResults(_selected_result);
 
-            foreach (Node node in ModelController.Results.Nodes.Values)
-            {
-                guidGsaKeys.Add(node.Id, node.GsaModelKey);
-            }
-
-            int gsaKeyRefNode = guidGsaKeys[ModelController.Results.StructureElements[0].Nodes[0].NodeId];
-            double x0 = ModelController.Results.Nodes[gsaKeyRefNode].X;
-            double y0 = ModelController.Results.Nodes[gsaKeyRefNode].Y;
-            HashSet<Guid> structureNodes = new HashSet<Guid>();
             Series series = new Series();
-            foreach (Element1d element1D in ModelController.Results.StructureElements)
-            {
-                foreach (ElementEndNode node in element1D.Nodes)
-                {
-                    if (!structureNodes.Contains(node.Id))
-                    {
-                        structureNodes.Add(node.NodeId);
-                    }
-                }
-            }
-            foreach (Guid guid in structureNodes)
-            {
-                if (ModelController.Results.Nodes.TryGetValue(guidGsaKeys[guid], out Node? value))
-                {
-                    double length = Math.Sqrt((value.X - x0) * (value.X - x0) + (value.Y - y0) * (value.Y - y0));
-                    double result = 0;
-                    switch (_selected_result)
-                    {
-                        case "Total displacement":
-                            result = GetValue(value.Displacement, _selected_direction);
-                            break;
-                        case "Stage displacement":
-                            result = GetValue(value.StageDisplacement, _selected_direction);
-                            break;
-                        case "Bending moment":
-                            result = value.BendingMoment ?? 0;
-                            break;
-                        case "Axial force":
-                            result = value.AxialForce ?? 0;
-                            break;
-                        case "Shear force":
-                            result = value.ShearForce ?? 0;
-                            break;
-
-                        default:
-                            break;
-                    }
-                    if (_invertAxis)
-                    {
-                        series.Points.AddXY(result, length);
-                    }
-                    else
-                    {
-                        series.Points.AddXY(length, result);
-                    }
-                }
-            }
             series.LegendText = _selected_result;
             series.ChartType = SeriesChartType.Line;
             series.MarkerStyle = MarkerStyle.Circle;
 
+            if (_invertAxis)
+            {
+                foreach (var point in nodalResults)
+                {
+                    series.Points.AddXY(point.Item2, point.Item1);
+                }
+            }
+            else
+            {
+                foreach (var point in nodalResults)
+                {
+                    series.Points.AddXY(point.Item1, point.Item2);
+                }
+            }
             ChartArea chartArea = chart1.ChartAreas.First();
-
             chartArea.AxisX.Minimum = series.Points.Min(x => x.XValue);
             chartArea.AxisX.Maximum = series.Points.Max(x => x.XValue);
             chartArea.AxisX.LabelStyle.Angle = 0;
@@ -218,94 +142,35 @@ namespace StructureResultsViewer
 
         private void PlotContactResults()
         {
-            if (ModelController == null)
-            {
-                return;
-            }
-            if (ModelController.Results == null)
+            if ((ModelController == null) || (ModelController.Results == null))
             {
                 return;
             }
 
-            Dictionary<Guid, int> guidGsaKeys = new Dictionary<Guid, int>(ModelController.Results.Nodes.Count);
-
-            foreach (Node node in ModelController.Results.Nodes.Values)
-            {
-                guidGsaKeys.Add(node.Id, node.GsaModelKey);
-            }
-
-            int gsaKeyRefNode = guidGsaKeys[ModelController.Results.StructureElements[0].Nodes[0].NodeId];
-            double x0 = ModelController.Results.Nodes[gsaKeyRefNode].X;
-            double y0 = ModelController.Results.Nodes[gsaKeyRefNode].Y;
+            List<(int, double, double)> contactResults = ModelController.ReadContactResults(_selected_result);
 
             int numSides = 2;
             Series[] series = new Series[2];
             series[0] = new Series();
             series[1] = new Series();
 
-            HashSet<Guid> structureNodes = new HashSet<Guid>();
+            if (_invertAxis)
+            {
+                foreach (var point in contactResults)
+                {
+                    series[point.Item1].Points.AddXY(point.Item3, point.Item2);
+                }
+            }
+            else
+            {
+                foreach (var point in contactResults)
+                {
+                    series[point.Item1].Points.AddXY(point.Item2, point.Item3);
+                }
+            }
+
             for (int side = 0; side < numSides; side++)
             {
-                foreach (Element1d element1D in ModelController.Results.StructureElements)
-                {
-                    ElementEndNode node0 = element1D.Nodes[0];
-                    ElementEndNode node1 = element1D.Nodes[1];
-
-                    (double, double) pointNode0 = (0, 0), pointNode1 = (0, 0), midPoint = (0, 0);
-                    if (ModelController.Results.Nodes.TryGetValue(guidGsaKeys[node0.NodeId], out Node? value0))
-                    {
-                        pointNode0 = (value0.X, value0.Y);
-                    }
-
-                    if (ModelController.Results.Nodes.TryGetValue(guidGsaKeys[node1.NodeId], out Node? value1))
-                    {
-                        pointNode1 = (value1.X, value1.Y);
-                    }
-                    midPoint = ((pointNode0.Item1 + pointNode1.Item1) / 2.0, (pointNode0.Item2 + pointNode1.Item2) / 2.0);
-
-                    for (int segment = 0; segment < 2; segment++)
-                    {
-                        ContactInterface CIResult = element1D.GetContactResult(side, segment);
-                        if (segment == 0)
-                        {
-                            double resultNode0 = GetResult(CIResult, 0);
-                            double resultMidNode = GetResult(CIResult, 1);
-
-                            double lengthNode0 = Math.Sqrt((pointNode0.Item1 - x0) * (pointNode0.Item1 - x0) + (pointNode0.Item2 - y0) * (pointNode0.Item2 - y0));
-                            double lengthMidNode = Math.Sqrt((midPoint.Item1 - x0) * (midPoint.Item1 - x0) + (midPoint.Item2 - y0) * (midPoint.Item2 - y0));
-
-                            if (_invertAxis)
-                            {
-                                series[side].Points.AddXY(resultNode0, lengthNode0);
-                                series[side].Points.AddXY(resultMidNode, lengthMidNode);
-                            }
-                            else
-                            {
-                                series[side].Points.AddXY(lengthNode0, resultNode0);
-                                series[side].Points.AddXY(lengthMidNode, resultMidNode);
-                            }
-                        }
-                        else
-                        {
-                            double resultMidNode = GetResult(CIResult, 0);
-                            double resultNode1 = GetResult(CIResult, 1);
-
-                            double lengthNode1 = Math.Sqrt((pointNode1.Item1 - x0) * (pointNode1.Item1 - x0) + (pointNode1.Item2 - y0) * (pointNode1.Item2 - y0));
-                            double lengthMidNode = Math.Sqrt((midPoint.Item1 - x0) * (midPoint.Item1 - x0) + (midPoint.Item2 - y0) * (midPoint.Item2 - y0));
-
-                            if (_invertAxis)
-                            {
-                                series[side].Points.AddXY(resultMidNode, lengthMidNode);
-                                series[side].Points.AddXY(resultNode1, lengthNode1);
-                            }
-                            else
-                            {
-                                series[side].Points.AddXY(lengthMidNode, resultMidNode);
-                                series[side].Points.AddXY(lengthNode1, resultNode1);
-                            }
-                        }
-                    }
-                }
                 series[side].LegendText = "Side " + side.ToString();
                 series[side].ChartType = SeriesChartType.Line;
                 series[side].MarkerStyle = MarkerStyle.Circle;
@@ -326,13 +191,84 @@ namespace StructureResultsViewer
                     chartArea.AxisY.Title = _selected_result + " " + _selected_direction;
                     chartArea.AxisX.Title = "Length";
                 }
-
                 chart1.Series.Add(series[side]);
                 chart1.Legends[side].Position.Auto = false;
                 chartArea.RecalculateAxesScale();
             }
 
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ModelController == null)
+            {
+                return;
+            }
+            if (ModelController.Results == null)
+            {
+                return;
+            }
+            if (ModelController.Results.StructureElements.Count == 0)
+            {
+                MessageBox.Show("The input json does not contain structure elements. Try another. ", "Nothing to show", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.InitialDirectory = "c:\\";
+                    saveFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+                    saveFileDialog.FilterIndex = 1;
+                    saveFileDialog.RestoreDirectory = true;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        if (_selected_result == "Reactions (contacts)" || _selected_result == "Relative displacement (contacts)" || _selected_result == "Steady pore-pressure (contacts)" | _selected_result == "Excess pore-pressure (contacts)")
+                        {
+                            List<(int, double, double)> contactResults = ModelController.ReadContactResults(_selected_result);
+                            List<(int, double, double)> contactResultsSide0 = contactResults.Where(x => x.Item1 == 0).OrderBy(x => x.Item2).ToList();
+                            List<(int, double, double)> contactResultsSide1 = contactResults.Where(x => x.Item1 == 1).OrderBy(x => x.Item2).ToList();
+                            using (StreamWriter writetext = new StreamWriter(saveFileDialog.FileName))
+                            {
+                                for (int i = 0; i < contactResultsSide0.Count; i++)
+                                {
+                                    if (contactResultsSide0[i].Item2 == contactResultsSide0[i].Item2)
+                                    {
+                                        writetext.WriteLine("{0},{1},{2}", contactResultsSide0[i].Item2, contactResultsSide0[i].Item3, contactResultsSide1[i].Item3);
+                                    }
+                                    else
+                                    {
+                                        throw new InvalidDataException("Data could not be organised for export");
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            List<(double, double)> nodalResults = ModelController.ReadNodalResults(_selected_result);
+                            using (StreamWriter writetext = new StreamWriter(saveFileDialog.FileName))
+                            {
+                                foreach (var point in nodalResults)
+                                {
+                                    writetext.WriteLine("{0},{1}", point.Item1, point.Item2);
+                                }
+                            }
+                        }
+                        MessageBox.Show("CSV file successfully saved!", "File output", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (System.ArgumentException)
+            {
+                MessageBox.Show("The input json has an unexpected schema", "Results Json required", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            
         }
     }
 }
